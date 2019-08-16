98c20d9751189ad79cfe2cd2aec61d614ba49eec
Philipp Ringele <p.ringele@asscope.com>
2019-08-16 17:10:49 +0200

    Save workday by creating workingentry

--- src/main/java/com/asscope/timesheet/service/WorkingEntryService.java
+++ src/main/java/com/asscope/timesheet/service/WorkingEntryService.java
@@ -69,6 +69,9 @@ public class WorkingEntryService {
             }
         	workingEntryToSave.setWorkDay(workDay);
         }
+        else if (workingEntryToSave.getWorkDay().getId() == null) {
+        	workingEntryToSave.setWorkDay(workDayService.save(workingEntryToSave.getWorkDay()));
+        }
         return workingEntryRepository.save(workingEntryToSave);
     }
 
