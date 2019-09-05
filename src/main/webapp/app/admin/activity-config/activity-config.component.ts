bc639ca44160eaaa0fb8b2775fe530645cd5f33b
Yash Vyas <y.vyas@asscope.com>
2019-09-05 17:19:42 +0200

    dialogs-for-activity-config

--- src/main/webapp/app/admin/activity-config/activity-config.component.ts
+++ src/main/webapp/app/admin/activity-config/activity-config.component.ts
@@ -2,11 +2,10 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { RoleTimesheetService } from 'app/entities/role-timesheet';
 import { RoleTimesheet, IRoleTimesheet } from 'app/shared/model/role-timesheet.model';
 import { HttpResponse } from '@angular/common/http';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivityTimesheet, IActivityTimesheet } from 'app/shared/model/activity-timesheet.model';
 import { ActivityTimesheetService } from 'app/entities/activity-timesheet';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
 @Component({
   selector: 'jhi-activity-config',
   templateUrl: './activity-config.component.html',
@@ -15,20 +14,13 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 export class ActivityConfigComponent implements OnInit {
   roles: RoleTimesheet[];
   roleForm: FormGroup;
-
+  rolemappingForm: FormGroup;
   activities: ActivityTimesheet[];
   panelOpenState = false;
   datasource = this.roles;
   displayedColumns: string[] = ['id', 'name', 'description', 'activities'];
-
   constructor(private activityService: ActivityTimesheetService, public dialog: MatDialog, private roleService: RoleTimesheetService) {}
-
   ngOnInit() {
-    this.roleForm = new FormGroup({
-      name: new FormControl(''),
-      description: new FormControl(''),
-      activities: new FormControl('')
-    });
     this.roleService.query().subscribe((res: HttpResponse<IRoleTimesheet[]>) => {
       if (res.ok) {
         this.roles = res.body;
@@ -41,50 +33,44 @@ export class ActivityConfigComponent implements OnInit {
     });
   }
   openroleDialog(): void {
-    const dialogRef = this.dialog.open(Dialogrolecreation, {
+    const dialogRef = this.dialog.open(activityroledialog, {
       width: '50%'
-      //  height: '50%',
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      //this.animal = result;
     });
   }
-
-  onsubmit(): void {
-    let role: IRoleTimesheet = <IRoleTimesheet>this.roleForm.value;
-    role.name = this.roleForm.value.name;
-    role.description = this.roleForm.value.description;
-    role.activities = null;
-    this.roleService.create(role).subscribe(res => {
-      if (res.ok) {
-        // TODO add res.body to the table
-      }
+  openactivityDialog(): void {
+    const dialogRef = this.dialog.open(activityCreationdialog, {
+      width: '50%'
+    });
+  }
+  openmappingDialog(): void {
+    const dialogRef = this.dialog.open(roleactivitymappingDialog, {
+      width: '50%'
     });
   }
 }
 @Component({
-  selector: 'dialog-rolecreation-dialog',
-  templateUrl: 'Dialogrolecreation.html'
+  selector: 'dialog-mapping',
+  templateUrl: 'activity-role-mapping-dialog.html'
 })
-export class Dialogrolecreation {
+export class roleactivitymappingDialog {
+  activities: ActivityTimesheet[];
+  roles: RoleTimesheet[];
+  mappingForm = new FormGroup({
+    role: new FormControl(''),
+    activities: new FormControl('')
+  });
   constructor(
     private roleService: RoleTimesheetService,
     private activityService: ActivityTimesheetService,
-    public dialogRef: MatDialogRef<Dialogrolecreation>,
-    @Inject(MAT_DIALOG_DATA) public data: IRoleTimesheet
+    public dialogRef: MatDialogRef<activityCreationdialog>,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: IActivityTimesheet
   ) {}
-  activities: ActivityTimesheet[];
-  rolemappingForm: FormGroup;
-  roles: RoleTimesheet[];
-  roleForm: FormGroup;
-
   ngOnInit() {
-    this.rolemappingForm = new FormGroup({
-      name: new FormControl(''),
-      description: new FormControl(''),
-      activities: new FormControl['']()
+    this.roleService.query().subscribe((res: HttpResponse<IRoleTimesheet[]>) => {
+      if (res.ok) {
+        this.roles = res.body;
+      }
     });
     this.activityService.query().subscribe((res: HttpResponse<IActivityTimesheet[]>) => {
       if (res.ok) {
@@ -93,11 +79,93 @@ export class Dialogrolecreation {
     });
   }
   onsubmit(): void {
-    let role: IRoleTimesheet = <IRoleTimesheet>this.roleForm.value;
-    role.name = this.roleForm.value.name;
-    role.description = this.roleForm.value.description;
-    role.activities = this.rolemappingForm.value.activities;
-    this.roleService.create(role).subscribe(res => {
+    let rolesub = <IRoleTimesheet>this.mappingForm.value.role;
+    let activities = <IActivityTimesheet[]>this.mappingForm.value.activities;
+    activities.forEach(element => {
+      rolesub.activities.push(element);
+    });
+    this.roleService.update(rolesub).subscribe(res => {
+      if (res.ok) {
+        // TODO add res.body to the table
+      }
+    });
+    console.log(this.mappingForm.value);
+    this.dialogRef.close();
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+@Component({
+  selector: 'dialog-activitycreation',
+  templateUrl: 'activity-creation-dialog.html'
+})
+export class activityCreationdialog {
+  activities: ActivityTimesheet[];
+  activityForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl('')
+  });
+  constructor(
+    private activityService: ActivityTimesheetService,
+    public dialogRef: MatDialogRef<activityCreationdialog>,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: IActivityTimesheet
+  ) {}
+  ngOnInit() {}
+  onsubmit(): void {
+    let activityEntry: ActivityTimesheet;
+    activityEntry = new ActivityTimesheet();
+    activityEntry.name = this.activityForm.value.name;
+    activityEntry.description = this.activityForm.value.description;
+    //  roleEntry.activities = this.rolemappingForm.value.activitiesform;
+    console.log(this.activityForm.value);
+    this.dialogRef.close();
+    this.activityService.create(activityEntry).subscribe(res => {
+      if (res.ok) {
+        // TODO add res.body to the table
+      }
+    });
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+@Component({
+  selector: 'dialog-rolecreation',
+  templateUrl: 'activity-role-dialog.html'
+})
+export class activityroledialog {
+  activities: ActivityTimesheet[];
+  roles: RoleTimesheet[];
+  rolemappingForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    activitiesform: new FormControl('')
+  });
+  constructor(
+    private roleService: RoleTimesheetService,
+    private activityService: ActivityTimesheetService,
+    public dialogRef: MatDialogRef<activityroledialog>,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: IRoleTimesheet
+  ) {}
+  ngOnInit() {
+    this.activityService.query().subscribe((res: HttpResponse<IActivityTimesheet[]>) => {
+      if (res.ok) {
+        this.activities = res.body;
+      }
+    });
+  }
+  onsubmit(): void {
+    let roleEntry: RoleTimesheet;
+    roleEntry = new RoleTimesheet();
+    roleEntry.name = this.rolemappingForm.value.name;
+    roleEntry.description = this.rolemappingForm.value.description;
+    //  roleEntry.activities = this.rolemappingForm.value.activitiesform;
+    console.log(this.rolemappingForm.value);
+    this.dialogRef.close();
+    this.roleService.create(roleEntry).subscribe(res => {
       if (res.ok) {
         // TODO add res.body to the table
       }
--- src/main/webapp/app/admin/admin.module.ts
+++ src/main/webapp/app/admin/admin.module.ts
@@ -4,10 +4,14 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { JhiLanguageHelper } from 'app/core';
 import { TimesheetSharedModule } from 'app/shared';
 import { EmployeeOverviewComponent } from './employee-overview/employee-overview.component';
-import { ActivityConfigComponent, Dialogrolecreation } from './activity-config/activity-config.component';
+import {
+  ActivityConfigComponent,
+  activityroledialog,
+  activityCreationdialog,
+  roleactivitymappingDialog
+} from './activity-config/activity-config.component';
 import { AsLayoutsModule } from 'app/as-layouts/as-layouts.module';
 /* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
-
 import {
   adminState,
   AuditsComponent,
@@ -18,17 +22,8 @@ import {
   JhiConfigurationComponent,
   JhiDocsComponent
 } from './';
-
 @NgModule({
-  imports: [
-    AsLayoutsModule,
-    TimesheetSharedModule,
-    AsLayoutsModule,
-    //  ActivityConfigComponent,
-
-    /* jhipster-needle-add-admin-module - JHipster will add admin modules here */
-    RouterModule.forChild(adminState)
-  ],
+  imports: [AsLayoutsModule, TimesheetSharedModule, AsLayoutsModule, RouterModule.forChild(adminState)],
   declarations: [
     AuditsComponent,
     LogsComponent,
@@ -39,12 +34,19 @@ import {
     JhiMetricsMonitoringComponent,
     EmployeeOverviewComponent,
     ActivityConfigComponent,
-    Dialogrolecreation
-    //R/olesComponent,
-    //ActivityComponent
+    activityroledialog,
+    ActivityConfigComponent,
+    activityCreationdialog,
+    roleactivitymappingDialog
   ],
   providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
-  entryComponents: [JhiHealthModalComponent, ActivityConfigComponent, Dialogrolecreation],
+  entryComponents: [
+    JhiHealthModalComponent,
+    ActivityConfigComponent,
+    activityroledialog,
+    activityCreationdialog,
+    roleactivitymappingDialog
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TimesheetAdminModule {
