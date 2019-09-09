e17cc4af9cae7824c9e0b38faf28a39fd92e1117
Philipp Ringele <p.ringele@asscope.com>
2019-09-09 16:57:04 +0200

    pdf table generation

--- src/main/webapp/app/shared/pdf/pdf.service.ts
+++ src/main/webapp/app/shared/pdf/pdf.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import { IWorkingEntryTimesheet } from '../model/working-entry-timesheet.model';
+import { Moment } from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,30 @@ export class PdfService {
   constructor() {}
 
   public createPDF(workingEntries: IWorkingEntryTimesheet[]): void {
+    let rows = workingEntries.map(we => [
+      we.workDay.date.format('YYYY-MM-DD'),
+      we.start.format('HH:mm'),
+      we.end.format('HH:mm'),
+      this.secondsToHHMM(we.end.diff(we.start, 'seconds', true)),
+      we.activity ? we.activity.name : ''
+    ]);
     let doc = new jsPDF();
-    workingEntries.forEach(we => {});
-    doc.text('Hello world!', 10, 10);
+    doc.autoTable({
+      head: [['Date', 'From', 'To', 'Worktime', 'Activity']],
+      body: rows
+    });
     doc.save('timesheet.pdf');
+  }
+
+  pad(num: number, size: number): string {
+    let s = num + '';
+    while (s.length < size) s = '0' + s;
+    return s;
+  }
+
+  secondsToHHMM(seconds: number): string {
+    const hour = Math.round(seconds / 3600);
+    const min = Math.round((seconds % 3600) / 60);
+    return this.pad(hour, 2) + 'h ' + this.pad(min, 2) + 'm';
   }
 }
