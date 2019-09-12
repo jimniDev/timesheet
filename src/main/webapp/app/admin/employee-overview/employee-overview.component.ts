e8e8bf16108ab93dfe2c51b94df0ac785bfd62a2
Yash Vyas <y.vyas@asscope.com>
2019-09-12 11:22:39 +0200

    pagination-employee-table #73

--- src/main/webapp/app/admin/employee-overview/employee-overview.component.ts
+++ src/main/webapp/app/admin/employee-overview/employee-overview.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployeeTimesheet, IEmployeeTimesheet } from 'app/shared/model/employee-timesheet.model';
 import { EmployeeTimesheetService, EmployeeTimesheetDetailComponent } from 'app/entities/employee-timesheet';
 import { HttpResponse } from '@angular/common/http';
 import { WeeklyWorkingHoursTimesheet } from 'app/shared/model/weekly-working-hours-timesheet.model';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { IRoleTimesheet } from 'app/shared/model/role-timesheet.model';
 
 @Component({
   selector: 'jhi-employee-overview',
@@ -12,20 +14,27 @@ import { WeeklyWorkingHoursTimesheet } from 'app/shared/model/weekly-working-hou
 export class EmployeeOverviewComponent implements OnInit {
   public employees: EmployeeTimesheet[];
   public totalWorkingHours: number;
+  datasource = new MatTableDataSource<EmployeeTimesheet>();
 
   constructor(private employeeService: EmployeeTimesheetService) {
     this.totalWorkingHours = 0;
   }
+
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
   ngOnInit() {
     this.employeeService.query().subscribe((res: HttpResponse<IEmployeeTimesheet[]>) => {
       if (res.ok) {
         this.employees = res.body;
         this.getActualWeeklyWorkingHours(this.employees);
+        this.datasource = new MatTableDataSource(this.employees);
+        this.datasource.paginator = this.paginator;
       }
     });
   }
-
+  applyFilter(filterValue: string) {
+    this.datasource.filter = filterValue.trim().toLowerCase();
+  }
   getActualWeeklyWorkingHours(employee: EmployeeTimesheet[]): void {
     if (employee) {
       for (let indexEmployee = 0; indexEmployee < employee.length; indexEmployee++) {
