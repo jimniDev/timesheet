ba0c02d1df3c7cc4ad99758350cd4d4bcc3488d6
Philipp Ringele <p.ringele@asscope.com>
2019-09-30 11:51:53 +0200

    auto login

--- src/main/webapp/app/admin/activity-config/activity-config.route.ts
+++ src/main/webapp/app/admin/activity-config/activity-config.route.ts
@@ -1,7 +1,13 @@
 import { ActivityConfigComponent } from './activity-config.component';
 import { Route } from '@angular/router';
+import { UserRouteAccessService } from 'app/core';
 
 export const activityconfigRoute: Route = {
   path: 'activity-config',
-  component: ActivityConfigComponent
+  component: ActivityConfigComponent,
+  data: {
+    authorities: ['ROLE_ADMIN'],
+    pageTitle: 'home.title'
+  },
+  canActivate: [UserRouteAccessService]
 };
