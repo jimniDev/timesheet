f8905a18003c59dbe2e036298bb179aa51e05b60
Philipp Ringele <p.ringele@asscope.com>
2019-10-09 18:40:13 +0200

    pdf

--- src/main/webapp/app/shared/pdf/pdf.service.ts
+++ src/main/webapp/app/shared/pdf/pdf.service.ts
@@ -1,27 +1,69 @@
-import { Injectable, ComponentFactoryResolver, Injector } from '@angular/core';
-import * as jsPDF from 'jspdf';
-import 'jspdf-autotable';
+import { Injectable } from '@angular/core';
 import { IWorkingEntryTimesheet } from '../model/working-entry-timesheet.model';
-import { TableGeneratorComponent } from './table-generator/table-generator.component';
+
+// import * as jsPDF from 'jspdf';
+// import 'jspdf-autotable';
+
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Injectable()
 export class PdfService {
-  constructor(private resolver: ComponentFactoryResolver, private injector: Injector) {}
+  constructor() {}
 
   public createPDF(workingEntries: IWorkingEntryTimesheet[]): void {
-    const rows = workingEntries.map(we => [
-      we.workDay.date.format('YYYY-MM-DD'),
-      we.start.format('HH:mm'),
-      we.end.format('HH:mm'),
-      this.secondsToHHMM(we.end.diff(we.start, 'seconds', true)),
-      we.activity ? we.activity.name : ''
-    ]);
-    const doc = new jsPDF();
-    doc.autoTable({
-      head: [['Date', 'From', 'To', 'Worktime', 'Activity']],
-      body: rows
-    });
-    doc.save('timesheet.pdf');
+    // const rows = workingEntries.map(we => [
+    //   we.workDay.date.format('YYYY-MM-DD'),
+    //   we.start.format('HH:mm'),
+    //   we.end.format('HH:mm'),
+    //   this.secondsToHHMM(we.end.diff(we.start, 'seconds', true)),
+    //   we.activity ? we.activity.name : ''
+    // ]);
+    // const doc = new jsPDF();
+    // doc.text(30, 10, 'Timesheet');
+    // doc.autoTable(40, {
+    //   head: [['Date', 'From', 'To', 'Worktime', 'Activity']],
+    //   body: rows
+    // });
+    // doc.text(20, 20, 'Sum');
+    // doc.save('timesheet.pdf');
+
+    const docDefinition = {
+      content: [
+        { text: 'AS Scope Timesheet - Philipp Ringele - October', fontSize: 15 },
+        {
+          layout: {
+            hLineWidth: (i, node) => 1, // (i === 0 || i === node.table.body.length) ? 2 : 1,
+            vLineWidth: (i, node) => 1, //  (i === 0 || i === node.table.widths.length) ? 2 : 1,
+            hLineColor: (i, node) => 'black', // (i === 0 || i === node.table.body.length) ? 'black' : 'gray',
+            vLineColor: (i, node) => 'black' // (i === 0 || i === node.table.widths.length) ? 'black' : 'gray',
+            // hLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
+            // vLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
+            // paddingLeft: function(i, node) { return 4; },
+            // paddingRight: function(i, node) { return 4; },
+            // paddingTop: function(i, node) { return 2; },
+            // paddingBottom: function(i, node) { return 2; },
+            // fillColor: function (rowIndex, node, columnIndex) { return null; }
+          },
+          table: {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: ['*', '*', '*', '*', '*'],
+
+            body: workingEntries.map(we => [
+              we.workDay.date.format('YYYY-MM-DD'),
+              we.start.format('HH:mm'),
+              we.end.format('HH:mm'),
+              this.secondsToHHMM(we.end.diff(we.start, 'seconds', true)),
+              we.activity ? we.activity.name : ''
+            ])
+          }
+        }
+      ]
+    };
+    pdfMake.createPdf(docDefinition).open({}, window);
   }
 
   pad(num: number, size: number): string {
