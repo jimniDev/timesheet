6908d2d8e5e51a57159cebf60400cda91ee4979e
Philipp Ringele <p.ringele@asscope.com>
2019-10-25 14:34:12 +0200

    css better

--- src/main/webapp/app/blocks/interceptor/auth-expired.interceptor.ts
+++ src/main/webapp/app/blocks/interceptor/auth-expired.interceptor.ts
@@ -10,32 +10,30 @@ export class AuthExpiredInterceptor implements HttpInterceptor {
   constructor(private stateStorageService: StateStorageService, private loginService: LoginService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next
-      .handle(request)
-      .pipe
-      // tap(
-      //   (event: HttpEvent<any>) => {},
-      //   (err: any) => {
-      //     if (err instanceof HttpErrorResponse) {
-      //       if ((err.status === 401 || err.status === 0) && err.url && !err.url.includes('api/account')) {
-      //         const destination = this.stateStorageService.getDestinationState();
-      //         if (destination !== null) {
-      //           const to = destination.destination;
-      //           const toParams = destination.params;
-      //           if (to.name === 'accessdenied') {
-      //             this.stateStorageService.storePreviousState(to.name, toParams);
-      //           }
-      //         } else {
-      //           this.stateStorageService.storeUrl('/');
-      //         }
+    return next.handle(request).pipe(
+      tap(
+        (event: HttpEvent<any>) => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if ((err.status === 401 || err.status === 0) && err.url && !err.url.includes('api/account')) {
+              const destination = this.stateStorageService.getDestinationState();
+              if (destination !== null) {
+                const to = destination.destination;
+                const toParams = destination.params;
+                if (to.name === 'accessdenied') {
+                  this.stateStorageService.storePreviousState(to.name, toParams);
+                }
+              } else {
+                this.stateStorageService.storeUrl('/');
+              }
 
-      //         this.loginService.login();
-      //         // } else if (err.status === 0) {
-      //         //   this.loginService.login();
-      //       }
-      //     }
-      //   }
-      // )
-      ();
+              this.loginService.login();
+              // } else if (err.status === 0) {
+              //   this.loginService.login();
+            }
+          }
+        }
+      )
+    );
   }
 }
